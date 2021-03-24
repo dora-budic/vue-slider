5ef9312b1dc993d7abb11541223bd29cb8613b69
Dora Budić <dbudic0706@gmail.com>
2021-03-24 15:12:15 +0100

    Change pic with prev,next

--- main.js
+++ main.js
@@ -1,9 +1,42 @@
 var app = new Vue ({
   el: "#root",
   data: {
-
+    index: 0,
+    imageSrc: [
+      './img/abu_dhabi.jpg',
+      './img/amsterdam.jpg',
+      './img/cappadocia.jpg',
+      './img/paris.jpg',
+      './img/prague.jpg',
+      './img/rio.jpg',
+      './img/rome.jpg',
+      './img/thailand.jpg'
+    ],
+    imageAlt: [
+      'abu dhabi mosque sheikh zayed',
+      'amsterdam',
+      'cappadocia balloons',
+      'paris',
+      'prague',
+      'rio de janeiro copacabana',
+      'rome piazza navona',
+      'thailand'
+    ]
   },
   methods: {
-    
+    next: function() {
+      if (this.index < this.imageSrc.length - 1) {
+        this.index++;
+      } else {
+        this.index = 0;
+      }
+    },
+    previous: function() {
+      if (this.index > 0) {
+        this.index--;
+      } else {
+        this.index = this.imageSrc.length - 1;
+      }
+    }
   }
 });
